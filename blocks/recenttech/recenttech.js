b7f4b9cde2949d2746dc5152b4c0b817945911d8
Mark Szulc <mszulc@adobe.com>
2023-06-04 13:16:06 +1000

    recenttech block

---
+++ blocks/recenttech/recenttech.js
@@ -0,0 +1,30 @@
+export default async function decorate(block) {
+  const indexResponse = await fetch('/query-index.json');
+  if (!indexResponse.ok) {
+    return;
+  }
+
+  const index = await indexResponse.json();
+
+  const container = document.createElement('ul');
+
+  index.data
+    .forEach((post) => {
+      if (post.category !== 'tech') {
+        return;
+      }
+      const li = document.createElement('li');
+
+      li.innerHTML = `
+      <a href="${post.path}">
+          <img src="${post.image}" alt="${post.title}" />
+          <h4>${post.title}</h4>
+      </a>
+    `;
+      container.append(li);
+    });
+
+
+
+  block.append(container);
+}
